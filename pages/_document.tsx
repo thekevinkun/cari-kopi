4f9c275b7d454afab513d33360bdfd13c0009eff
thekevinkun <developer.kevinkun@gmail.com>
2025-07-08 17:25:46 +0800

    Remove depecrated meta tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -21,7 +21,6 @@ export default function Document() {
         <link rel="manifest" href="/manifest.webmanifest" />
 
         <meta name="theme-color" content="#804A26" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
           name="apple-mobile-web-app-status-bar-style"
           content="black-translucent"
